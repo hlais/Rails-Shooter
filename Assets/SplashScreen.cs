ba2f15a76fe83cf4caa6b2ed56ac2563ee76aa61
Halim Lais <halimlais@hotmail.com>
2018-10-02 08:00:04 +0100

    Rails Shooter

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SplashScreen : MonoBehaviour {
+
+   
+    // Use this for initialization
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start () {
+        Invoke("LoadNextScreen", 3);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+		
+	}
+    void LoadNextScreen()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+
+        
+    }
+    
+}
